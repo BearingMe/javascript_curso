e40d6c386f39c80dcd759af6f548c55b24c8be9a
BearingMe <gomes.bruno201@gmail.com>
2022-02-11 12:00:50 -0300

    aula 02: tipos primitivos

---
+++ section 02/aula 02/index.js
@@ -0,0 +1,36 @@
+// string
+const name = 'Luiz';
+
+// number
+const age = 27;
+
+// boolean
+const hasHobbies = true;
+
+// undefined
+const job = undefined;
+
+// null
+const car = null;
+
+// object
+const address = {
+  city: 'São Paulo',
+  country: 'Brasil'
+};
+
+// array
+const hobbies = ['Sports', 'Cooking'];
+
+// function
+const calculateAge = (birthYear) => { return 2020 - birthYear; };
+
+// template string
+const hello = `Hello, my name is ${name} and I am ${age} years old.`;
+
+// object destructuring
+const { city, country } = address;
+
+// array destructuring
+const [hobby1, hobby2] = hobbies;
+
