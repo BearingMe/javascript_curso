9f71ae1135194c415fe1822677ffecad63c0212b
BearingMe <gomes.bruno201@gmail.com>
2022-02-11 12:02:42 -0300

    aula 06: template strings

---
+++ section 02/aula 06/index.js
@@ -0,0 +1,15 @@
+// declara e depois inicia a variavel
+let personagem;
+personagem = "Henrique";
+
+console.log(`${personagem} nasceu em 1984`);
+console.log(`Em 2000 ${personagem} conheceu Maria`);
+console.log(`Maria teve um filho com ${personagem} em 2002`);
+console.log(`O filho de ${personagem} se chama Eduardo`);
+
+// case sensitive
+let nome = "Henrique";
+let NOME = "Maria";
+
+console.log(nome);
+console.log(NOME);
