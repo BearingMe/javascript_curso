1f3a6c04467f946688f3a6e143e4dacdca57de3d
BearingMe <gomes.bruno201@gmail.com>
2022-02-21 05:50:02 -0300

    aula 34: for loop clássico

---
+++ section 03/aula 34/index.js
@@ -0,0 +1,8 @@
+// for loop clássico
+
+// variável que recebe o valor de 0
+// condição que verifica se o valor de i é menor que 10
+// incremento de 1
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
