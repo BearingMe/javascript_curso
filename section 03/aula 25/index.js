cfcc4609c9906d982a85efb73ad6e7eab6f1a71f
BearingMe <gomes.bruno201@gmail.com>
2022-02-21 05:43:44 -0300

    aula 25: exercicio

---
+++ section 03/aula 25/index.js
@@ -0,0 +1,11 @@
+const numero = 5;
+
+if (numero >= 0 && numero <= 5) {
+  console.log("O número está entre 0 e 10");
+} else if (numero >= 6 && numero <= 10) {
+  console.log("O número está entre 6 e 10");
+} else {
+  console.log("O número não está entre 0 e 10");
+}
+
+console.log("Fim do programa");
