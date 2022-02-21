2187bd88ca04556daf51b1abd704c94310e40d10
BearingMe <gomes.bruno201@gmail.com>
2022-02-21 05:41:50 -0300

    aula 22: operadores lógicos

---
+++ section 03/aula 22/index.js
@@ -0,0 +1,12 @@
+/**
+ * Operadores lógicos 
+ * && e -> Todas as operações precisam ser true para retornar true
+ * || ou -> Uma das operações precisam ser true para retornar true
+ * ! negação
+*/
+
+`eu tenho dinheiro? estou com fome?`
+console.log(true && true); // true
+
+`eu tenho dinheiro? está sol lá fora?`
+console.log(true || false); // true
